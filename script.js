2663d6a10b4b1bc7324cd5e4fd8df6d5eb0083e9
Utku Cikmaz <utkucikmaz@gmail.com>
2023-08-15 11:15:18 +0200

    fixed the eventlistener bugs(they weren't working at all) put all items into the room array

--- script.js
+++ script.js
@@ -10,85 +10,96 @@ class Game {
             goblins: 0,
         };
     }
-    showName() {
-        // const nameForm = document.createElement("form");
-        // nameForm.id = "nameForm";
-        // nameForm.style.backgroundColor = "purple";
-        // nameForm.innerHTML = `
-        // 	<label for="nameInput">Enter your name</label>
-        // 	<br/>
-        //     <input type="text" id="nameInput" placeholder="Your name" required>
-        // 	<br/>
-        //     <input id="submit-btn" type="submit" value="Start Game">`;
 
-        // const parentElm = document.getElementById("board");
-        // parentElm.appendChild(nameForm);
-        // nameForm.addEventListener("submit", (event) => {
-        //     event.preventDefault();
-        // });
-        this.startGame();
+    showName() {
+        const nameForm = document.createElement("form");
+        nameForm.id = "nameForm";
+        nameForm.style.backgroundColor = "purple";
+        nameForm.innerHTML = `
+        	<label for="nameInput">Enter your name</label>
+        	<br/>
+            <input type="text" id="nameInput" placeholder="Your name" required>
+        	<br/>
+            <input id="submit-btn" type="submit" value="Start Game">`;
+
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(nameForm);
+        nameForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            this.startGame();
+        });
     }
     startGame() {
-        // const nameInput = document.getElementById("nameInput");
-        // const userName = nameInput.value.trim().toUpperCase();
+        const nameInput = document.getElementById("nameInput");
+        const userName = nameInput.value.trim().toUpperCase();
 
-        // if (userName === "") {
-        //     alert("Please enter a valid name.");
-        //     return;
-        // }
+        if (userName === "") {
+            alert("Please enter a valid name.");
+            return;
+        }
 
-        // const nameForm = document.getElementById("nameForm");
-        // nameForm.style.display = "none";
+        const nameForm = document.getElementById("nameForm");
+        nameForm.style.display = "none";
         this.hotel = new Hotels("0", "0");
         this.hotel = new Hotels("30", "0");
         this.hotel = new Hotels("60", "0");
         this.hotel = new Hotels("90", "0");
 
-        this.initGameLogic();
+        this.createVisitors();
     }
-    initGameLogic() {
+    createVisitors() {
         let score = 0;
-        const room = [];
-        const visitorsArr = [];
 
         const parentElm = document.getElementById("board");
         const scoreDisplay = document.createElement("span");
         scoreDisplay.id = "scoreDisplay";
         parentElm.appendChild(scoreDisplay);
-        // const nameInput = document.getElementById("nameInput");
-        // const userName = nameInput.value.trim().toUpperCase();
+        const nameInput = document.getElementById("nameInput");
+        const userName = nameInput.value.trim().toUpperCase();
 
+        const generateVisitors = () => {
+            for (let i = 0; i < 4; i++) {
+                const newVisitor = new Visitors(this);
+                this.visitorsArr.push(newVisitor);
+                updateVisitorCount(newVisitor.type);
+                score++;
+                scoreDisplay.textContent = `Score: ${score}`;
+            }
+        };
         const updateVisitorCount = (type) => {
             console.log("Visitor Counts:", this.visitorCounts);
             return this.visitorCounts[type]++;
         };
 
-        const visitorsCreate = setInterval(() => {
-            const newVisitor = new Visitors(this);
-            visitorsArr.push(newVisitor);
-            updateVisitorCount(newVisitor.type);
-            score++;
-            scoreDisplay.textContent = `Score: ${score}`;
-        }, 2000);
-
         const visitorsMove = setInterval(() => {
-            visitorsArr.forEach((visitorInstance) => {
+            this.visitorsArr.forEach((visitorInstance) => {
                 visitorInstance.moveDown();
             });
         }, 50);
 
-        function cleaning() {
-            visitorsArr.forEach((visitorItem) => {
-                if (visitorItem.type === "human") {
-                    room.push(visitorItem);
-                    console.log(visitorItem);
-                    visitorItem.addEventListener("click", function () {
-                        clearInterval(visitorsCreate);
-                    });
-                }
+        const cleanUpVisitors = () => {
+            this.visitorsArr.forEach((visitor) => {
+                parentElm.removeChild(visitor.domElement); // Remove visitor DOM element
             });
-        }
-        cleaning();
+            this.visitorsArr = []; // Clear the visitors array
+        };
+
+        const generateAndClean = () => {
+            generateVisitors();
+            setTimeout(() => {
+                cleanUpVisitors();
+            }, 5000);
+        };
+
+        generateAndClean();
+        setInterval(generateAndClean, 10000); // Call the function every 10 seconds (5s for generation + 5s for cleaning)
+    }
+    handleVisitorClick(visitor) {
+        this.room.push(visitor);
+        console.log(
+            "Types in room:",
+            this.room.map((visitor) => visitor.type)
+        );
     }
 }
 class Hotels {
@@ -115,17 +126,19 @@ class Hotels {
     }
 }
 class Visitors {
-    constructor() {
+    constructor(game) {
         this.width = 10;
         this.height = 10;
         this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
         this.positionY = Math.floor(
             Math.random() * (100 - this.height - this.height + 1) + this.height
         );
+        this.game = game;
         this.createDomElement();
     }
 
     createDomElement() {
+        const parentElm = document.getElementById("board");
         this.domElement = document.createElement("button");
         this.domElement.setAttribute("id", "visitor");
         //this.domElement.getElementById("visitor");
@@ -134,8 +147,12 @@ class Visitors {
         this.domElement.style.left = this.positionX + "vw";
         this.domElement.style.bottom = this.positionY + "vh";
 
-        const parentElm = document.getElementById("board");
         parentElm.appendChild(this.domElement);
+        const self = this;
+
+        this.domElement.addEventListener("click", function () {
+            self.game.handleVisitorClick(self);
+        });
 
         let randomNumber = Math.floor(Math.random() * 4) + 1;
 
